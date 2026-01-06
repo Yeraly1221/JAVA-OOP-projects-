cf0368afe3a5b3bea007a4e9ce03a95e591c22f2
Yeraly1221 <email@example.com>
2026-01-06 16:58:24 +0500

    Initial commit

---
+++ Music/HDL_Assignment_Solutions-master/README.md
@@ -0,0 +1,2 @@
+# HDL Assignment Solutions
+This repository includes assignment solutions  of **Hardware Description Languages for FPGA Design** course which provided by University of Colorado Boulder on Coursera.
---
+++ Music/Python-Platformer-main/main.py
@@ -0,0 +1,137 @@
+import os 
+import random
+import math 
+import pygame 
+from os import listdir 
+#Gives a list of files and folders inside a directory
+from os.path import isfile, join
+#isfile:Checks if a path is a file 
+#join:Correctly joins folder + file name
+pygame.init()#start/init pygame
+
+# Set up the game window
+pygame.display.set_caption("YERAdone")
+
+BG_COLOR = (200, 222, 255)
+WIDTH, HEIGHT = 999, 777
+FPS = 60
+PLAYER_VEL = 8
+#player velocity
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.diraction = "right"
+        self.animation_count = 0
+        self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy 
+        
+
+    def move_left(self, vel):
+        self.x_vel = -vel 
+        if self.direction != "left":
+            self.diraction = "left"
+            self.animation_count = 0
+    
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.diraction = "right"
+            self.animation_count = 0
+    
+
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+        self.fall_count += 1
+
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
+
+
+
+
+
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    x, y, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+    return tiles, image
+
+
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+
+    player.draw(window)
+    
+    pygame.display.update()
+
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+
+    player.x_vel = 0
+    player.y_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Yellow.png")
+
+    player = Player(100, 100, 50, 50)
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
+    pygame.quit()
+    quit()        
+
+    
+
+if __name__ == "__main__":
+    main(window)
